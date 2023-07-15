92e2648d8eac9a09419ff272703a7ddb656c76e0
formaerf0 <formaerf0@gmail.com>
2023-07-15 16:16:32 -0600

    cambios

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebaseConfig = {
+  apiKey: "AIzaSyCAfngSJ7dDhfmEttaMVzx3TRb48HvUOLs",
+  authDomain: "autic33erf-e9815.firebaseapp.com",
+  projectId: "autic33erf-e9815",
+  storageBucket: "autic33erf-e9815.appspot.com",
+  messagingSenderId: "745761552896",
+  appId: "1:745761552896:web:220d7c61db66f526fd76e5",
+  measurementId: "G-6LP1KP88MY"
+};
